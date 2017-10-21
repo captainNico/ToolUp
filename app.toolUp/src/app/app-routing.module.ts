8eac4d641a1ff96908f6598eb6a690d2626e4a83
Nicolas Michot <nicolas.michot@yahoo.com>
2017-10-21 17:34:59 +0200

    Create the page login and signup

--- app.toolUp/src/app/app-routing.module.ts
+++ app.toolUp/src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Pages
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
+    },
+    {
+        path: '**', // Error 404
+        component: LoginComponent
+    }
 ];
 
 @NgModule({
