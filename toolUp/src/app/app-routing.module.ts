6c2d7b8ddc95afd2137e4398d0904e296d6c069f
Nicolas Michot <nicolas.michot@yahoo.com>
2017-10-21 16:08:16 +0200

    Create the home page

--- toolUp/src/app/app-routing.module.ts
+++ toolUp/src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Import pages
+import { HomeComponent } from './pages/home/home.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+    {
+        path: '',
+        component: HomeComponent,
+        children: []
+    },
+    {
+        path: '**', // For error 404
+        component: HomeComponent
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
